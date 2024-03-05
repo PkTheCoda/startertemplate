f4378d502dda205ff904caab0c2bd5d6f4b6ec81
PkTheCoda <pranavkonjeti@gmail.com>
2024-03-05 09:54:17 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="bg-red-100 six-title text-center font-fig">hi</div>
+    </>
+  )
+}
+
+export default App
